f451e13bd2ebeb67d5cc5c6e241662db6b47a3c0
Richard Depierre <richard.depierre@teads.tv>
2025-03-20 22:49:51 +0100

    start

---
+++ dqn.py
@@ -0,0 +1,20 @@
+import flappy_bird_gymnasium
+import gymnasium
+import torch
+from torch import nn
+import torch.nn.functional as F
+from collections import deque
+import random
+import itertools
+import yaml
+
+class DQN(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=256):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
